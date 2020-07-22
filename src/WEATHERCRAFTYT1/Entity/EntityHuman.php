b60a0ca6bcfc6243fe496b8626e7aa7e98d80358
WEATHERCRAFTYT1 <54820839+WEATHERCRAFTYT1@users.noreply.github.com>
2020-07-22 17:23:25 -0400

    Create EntityHuman.php

---
+++ src/WEATHERCRAFTYT1/Entity/EntityHuman.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace WEATHERCRAFTYT1\Entity\types;
+use pocketmine\{Server, Player, entity\Human};
+
+class EntityHuman extends Human {
+
+    public function getName() : string {
+        return '';
+    }
+}
+?>
